e7799ba873fd056645a8916163a23585a960d48a
mikky-j <moregunwa@gmail.com>
2024-02-05 08:54:07 +0100

    Cleaned up the code and made an example

--- src/certify_v1_frontend/src/index.tsx
+++ src/certify_v1_frontend/src/index.tsx
@@ -1,40 +1,14 @@
-import "./main.css";
 import React from "react";
 import { render } from "react-dom";
-const MyHello = () => {
-  const [name, setName] = React.useState("");
-  const [message, setMessage] = React.useState("Test");
+import App from "./App";
+import "./main.css";
 
+const Index = () => {
   return (
-    <div style={{ fontSize: "30px" }}>
-      <div className="bg-red-500">
-        <p>Greetings, from DFINITY!</p>
-        <p>
-          {" "}
-          Type your message in the Name input field, then click{" "}
-          <b> Get Greeting</b> to display the result.
-        </p>
-      </div>
-      <div style={{ margin: "30px" }}>
-        <input
-          id="name"
-          value={name}
-          onChange={(ev) => setName(ev.target.value)}
-        ></input>
-      </div>
-      <div>
-        Greeting is: "<span style={{ color: "blue" }}>{message}</span>"
-      </div>
-    </div>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   );
 };
 
-const App = () => {
-  return (
-    <div>
-      <MyHello />
-    </div>
-  );
-};
-
-render(<App />, document.getElementById("app"));
+render(<Index />, document.getElementById("app"));
